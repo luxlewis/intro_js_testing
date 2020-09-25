976a22d5ad8e82720f03185f56166ec6a82a7875
luxlewis <51483735+luxlewis@users.noreply.github.com>
2020-09-25 12:34:41 +0100

    Inital commit

--- tweet.js
+++ tweet.js
@@ -1,5 +1,23 @@
 class Tweet {
-
+  constructor(sender, content, likes, retweets, replies, replyCount) {
+  this.sender = sender;
+  this.content = content;
+  this.likes = 0;
+  this.retweets = 0;
+  this.replies = [];
+  this.replyCount = 0;
+  }
+  like() {
+    this.likes += 1;
+  }
+  retweet() {
+    this.retweets += 1;
+  }
+  reply(reply) {
+    this.replies.push(reply);
+    this.replyCount += 1;
+  }
 }
+
 
 module.exports = Tweet;
